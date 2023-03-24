9737a065d845f6efd0514ae3e50cba1eaee6a98c
Oscar Leal <oscarleal234@gmail.com>
2023-03-23 22:28:22 -0500

    succesfully fetching from both APIs

--- Assets/js/secondpage.js
+++ Assets/js/secondpage.js
@@ -2,7 +2,8 @@ let englishName = $("#englishName");
 let scienceName = $("#scienceName");
 let countryRec = $("#countryRec");
 let cityRec = $("#cityRec");
-
+let downlink = $("#downBtn");
+let imgHolder = $("#fetchedPhoto");
 $(document).ready(function () {
   // Retrieved stored data from local storage
   let birdData = JSON.parse(localStorage.getItem("birdData"));
@@ -12,9 +13,24 @@ $(document).ready(function () {
   scienceName.text(birdData.recordings[0].sp);
   countryRec.text(birdData.recordings[0].cnt);
   cityRec.text(birdData.recordings[0].loc);
+  downlink.attr("href", birdData.recordings[0].file);
+
+  let birdValue = birdData.recordings[0].en;
+
+  fetch("https://api.pexels.com/v1/search?query=" + birdValue, {
+    headers: {
+      Authorization: "FuhXBroha0FVf7V25shqXMuFhsGQYgxaA0dIL0esUNLnwAR7hJ3ZNhVa",
+    },
+  })
+    .then((resp) => {
+      return resp.json();
+    })
+    .then((data) => {
+      console.log(data);
+      imgHolder.attr("src", data.photos[0].src.original);
+    });
 });
 
 //TODO
 
-//1.CREATE A PLAY BUTTON
-//2. FETCH AUDIO RECORDING AND FIGURE OUT HOW TO SET IT TO PLAY BUTTON AND THEN ACTUALLY HAVE IT PLAY!!!
+//1.
